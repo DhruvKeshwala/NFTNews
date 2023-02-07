b5b163efc5479312b909594169cf8ef9955ceefb
Kishan Gareja <kishangareja241@gmail.com>
2023-02-07 18:36:59 +0530

    Changes on videos align at home page user side done

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -3,6 +3,12 @@
 @section('title', 'NFT Markets | Home')
 
 @section('content')
+<style>
+iframe {
+  width: 98%;
+  height: 600px;
+}
+</style>
     <section class="ftco-section px-0 p-0">
 
         <div class="d-md-flex">
@@ -756,12 +762,12 @@
 <!-- Quick View -->
     @foreach($videos as $video)
     <div class="modal fade" id="myModal-{{@$video->id}}" role="dialog" tabindex="-1" aria-hidden="true">
-        <div class="modal-dialog" style="top:15%; max-width: 60%;" role="document">  
+        <div class="modal-dialog" style="top:5%; max-width: 100%;" role="document">  
           <!-- Modal content-->     
-          <div class="modal-content">
+          <div class="modal-content" style="height: 800px;">
             <div class="modal-header">
-              {{-- <h6 class="modal-title"><i class="fa fa-calendar"></i>{{@$video->created_at->format('F d, Y')}}</h6> --}}
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h3>{{ @$video->title }}</h3>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
             </div>
             <div class="modal-body text-center">
                 {{-- <h4>{{@$video->title}}</h4><br> --}}
